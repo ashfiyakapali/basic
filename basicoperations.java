b2d2c3b9b997f710533fb4f753dd155ef5299269
ashfiyakapali <ashfiyakapali@gmail.com>
2025-05-06 09:55:22 +0530

    save

---
+++ basicoperations.java
@@ -0,0 +1,12 @@
+package basicoperations;
+
+public class basicoperations {
+        public static void main(String[] args) {
+            // Arithmetic Operations
+            int a = 10, b = 5;
+            System.out.println("Addition: " + (a + b));
+            System.out.println("Subtraction: " + (a - b));
+            System.out.println("Multiplication: " + (a * b));
+    
+}
+}
