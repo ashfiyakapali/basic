2e8f276987477ae75676befd38e1e1d1ed8bbd34
ashfiyakapali <ashfiyakapali@gmail.com>
2025-05-06 11:25:06 +0530

    Update basicoperations.java

--- basicoperations.java
+++ basicoperations.java
@@ -3,9 +3,12 @@ public class basicoperations {
         public static void main(String[] args) {
             // Arithmetic Operations
             int a = 10, b = 5;
-            System.out.println("Addition: " + (a + b));
-            System.out.println("Subtraction: " + (a - b));
-            System.out.println("Multiplication: " + (a * b));
+                int sum=a+b;
+                int diff=a-b;
+                int prod=a*b;
+            System.out.println("Addition: " + sum);
+            System.out.println("Subtraction: " + diff);
+            System.out.println("Multiplication: " + prod);
     
 }
 }
